364f9e5169d3f7f9b2a6dd8b63b5b4f71139777e
KUGAKU <githubgemini.0521@gmail.com>
2020-03-29 16:32:20 +0900

    implement first Dart pacjages

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/yamamotogaku/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/yamamotogaku/AndroidStudioProjects/xylophone-flutter"
+export "FLUTTER_TARGET=/Users/yamamotogaku/AndroidStudioProjects/xylophone-flutter/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/yamamotogaku/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
